7c83148ab45d66ab12b8f6bd0894453457363ce8
tessaSAC <tessaSAC@users.noreply.github.com>
2019-09-03 01:17:07 -0400

    adds markdown, prettier, and stylelint rc's

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf',
+}
