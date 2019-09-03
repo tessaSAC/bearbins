ac8bff3cda71968e58c01c579e8cb362ea6bb60e
tessaSAC <tessaSAC@users.noreply.github.com>
2019-09-03 01:17:16 -0400

    adds preliminary bear styles

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+<div id="app">
+  <div class="bearBin">
+    <div class="dumpster"><div class="clean bear" /></div>
   </div>
+  <div class="bearBin">
+    <div class="dumpster"><div class="clean bear" /><div class="dirty bear" /></div>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
+  name: 'app'
+}
 </script>
 
 <style lang="scss">
+$bearHeight: 4vw;
+$bearWidth: 5vh;
+
+$dumpsterWidth: 25vw;
+$dumpsterHeight: 10vh;
+
+.bearBin {
+  display: flex;
+  flex-direction: column;
+
+  + .bearBin { margin-left: 5vh; }
+}
+
+.dumpster {
+  display: flex;
+  justify-content: space-around;
+  background: green;
+  width: $dumpsterWidth;
+  height: $dumpsterHeight;
+}
+
+.bear {
+  background: brown;
+  width: $bearWidth;
+  height: $bearHeight;
+
+  position: relative;
+  bottom: $bearHeight;
+
+  &.dirty {
+    background: red;
+  }
+}
+
 #app {
+  // dumpster styles:
+  display: flex;
+
+
+
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
