848cb5edc93a699f254347b0e5f2266d0dd2b819
tessaSAC <tessaSAC@users.noreply.github.com>
2019-09-21 13:05:33 -0600

    adds test to see what the difference is between children wrapped in observers and those not (see notes from visual i)

---
+++ src/components/EffectsOfNonReactiveSubObjects.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data: _ => ({
+    someArr: []
+  }),
+
+  methods: {
+    // Experiment:
+    // Add sub-array
+    // Add sub-sub arrays (whether reactive or not)
+    // Notice that the reactive sub-sub arrays' contents continue to trigger changes
+    // Where non-reactive sub-sub arrays continue to be non-reactive
+
+    addSubArray() { this.someArr.push([]) },
+
+    addReactiveSubArray() { this.someArr[this.someArr.length - 1].push({ a: 1 })},
+    addNonReactiveSubArray() {
+      const currentSubArray = this.someArr[this.someArr.length - 1]
+      currentSubArray[currentSubArray.length] = { a: 1 }
+    },
+
+    alterSubArray() { this.someArr[this.someArr.length - 1][0].a += this.someArr[0][0].a }
+  }
+}
+</script>
+
+<template>
+<div>
+  <pre>
+    {{ someArr }}
+  </pre>
+  <button @click="addSubArray">add sub-array</button>
+  <button @click="addReactiveSubArray">add reactive sub-sub array</button>
+  <button @click="addNonReactiveSubArray">add non-reactive sub-sub array</button>
+  <button @click="alterSubArray">alter first sub-sub array in most recent sub-array</button>
+</div>
+</template>
